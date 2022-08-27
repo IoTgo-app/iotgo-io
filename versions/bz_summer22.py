08d5b14b8a37e0fa21f9bc0c66bae341493e3925
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-08-27 21:20:13 +0200

    test

--- versions/bz_summer22.py
+++ versions/bz_summer22.py
@@ -569,7 +569,7 @@ package_suffix = {
  
 
 on_end = {
-  	"recieveData":'\nradio.onReceivedValue(function (name, value){\n\t if (name == "replace" && value == 1){\n\t\t received = 1\n\t}\n})\n\telse if (name == "replace" && value == 0){\n\t\t received = 0\n\t}\n})}',
+  	"recieveData":'\nradio.onReceivedValue(function (name, value){\n\t if (name == "replaceaaa" && value == 1){\n\t\t received = 1\n\t}\n})\n\telse if (name == "replace" && value == 0){\n\t\t received = 0\n\t}\n})}',
     #was: '\nradio.onReceivedValue(function (name, value) {\n\tif (name == "replace" && value == 1) {\n\t\t received = 1\n\t}\n})',
         } 
 
