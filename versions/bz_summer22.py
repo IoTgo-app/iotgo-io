a31032ba6b1478319a2281e77a6b6d8a34fe6499
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-08-27 21:27:47 +0200

    fixed mismatched brackets in JS code

--- versions/bz_summer22.py
+++ versions/bz_summer22.py
@@ -569,7 +569,8 @@ package_suffix = {
  
 
 on_end = {
-  	"recieveData":'\nradio.onReceivedValue(function (name, value){\n\t if (name == "replaceaaa" && value == 1){\n\t\t received = 1\n\t}\n})\n\telse if (name == "replace" && value == 0){\n\t\t received = 0\n\t}\n})}',
+  	"recieveData":'\nradio.onReceivedValue(function (name, value){\n\t if (name == "replace" && value == 1){\n\t\t received = 1\n\t}\n}\n\t else if (name == "replace" && value == 0){\n\t\t received = 0\n\t}\n}})',
+
     #was: '\nradio.onReceivedValue(function (name, value) {\n\tif (name == "replace" && value == 1) {\n\t\t received = 1\n\t}\n})',
         } 
 
