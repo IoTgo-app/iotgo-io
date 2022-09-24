89cdbb0e1dc536ab36992d3b9746f17cffb8af5e
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-24 22:37:57 +0200

    Create inputs_IT.py

---
+++ webapp_v3/inputs_IT.py
@@ -0,0 +1,54 @@
+microbitv1= ("l\'accelerazione è alta" ,
+		    "l\'accelerazione è basso" ,
+		    "il pulsante non è premuto",
+		    "il pulsante è premuto",
+		    "la bussola punta ad Est",
+		    "la bussola punta a Nord" ,
+		    "la bussola punta a Sud" ,
+		    "la bussola punta ad Ovest" ,
+		    "il gesto è scuotere" ,
+		    "il gesto è inclinare" ,
+		    "l\'intensità di luce è alta",
+		    'l\'intensità di luce è bassa',
+		    'la temperatura è alta' ,
+		    'la temperatura è bassa' ,
+	
+		   )
+microbitv2= ( "il rumore è alto" ,
+		    'il rumore è basso' ,
+		    'il logo non è toccato', #v2 
+		    'il logo è toccato' ,#v2 
+		   )
+exBosonKit= ('non c\'è movimento nei dintorni (BosonKit)' ,
+'c\'è movimento nei dintorni (BosonKit)' ,
+'il cursore è al massimo (BosonKit)' ,
+'il cursore è al minimo (BosonKit)' ,
+#"il cursore è al medio (BosonKit)" ,
+		   )
+exEnviroBit= ("c\'è tanta umidità (Envirobit)",
+"c\'è poca umidità (Envirobit)",
+"la pressione atmosferica è alta (Envirobit)",
+"la pressione atmosferica è bassa (Envirobit)",
+'il rumore è alto (Envirobit)' ,
+'Il rumore è basso (Envirobit)' ,
+"la temperatura è alta (Envirobit)" ,
+"la temperatura è bassa (Envirobit)" ,
+"l\'intensità di luce è alta (Envirobit)",
+"l\'intensità di luce è bassa (Envirobit)",
+"il colore  è rosso (Envirobit)",
+"il colore  è verde (Envirobit)",
+"il colore  è blu (Envirobit)",
+"il colore  è nero (Envirobit)",
+"c\'è un applauso (Envirobit)",
+"non c’è un applauso (Envirobit)",
+)
+
+
+exCloudBitPi= ()
+
+exOthers= ("l\'umidità del suolo è bassa",
+"l\'umidità del suolo è alta",
+)
+
+
+p2p= ('recezione dati',)
