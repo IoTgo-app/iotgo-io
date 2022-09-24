38bf0e96b1850add45b65e2234634c94ece92b94
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-25 01:49:51 +0200

    cleanup

--- webapp_v3/main.py
+++ webapp_v3/main.py
@@ -19,6 +19,7 @@ st.markdown(mods.hide_img_fs, unsafe_allow_html=True)
 st.markdown(mods.fix_sidebar,unsafe_allow_html=True)
 st.markdown(mods.fix_tabs, unsafe_allow_html=True)
 st.markdown(mods.hide_top_padding, unsafe_allow_html=True)
+
 #################################################################################à
 langPrefix=['EN','IT','DE','UR']
 lang=1
