9e48829ba05eeca2d260e823a2114ca80a10fb6d
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-24 22:37:31 +0200

    Create outputs_IT.py

---
+++ webapp_v3/outputs_IT.py
@@ -0,0 +1,42 @@
+microbitv1= ('suona una melodia allegra' ,
+'smette di suonare' ,
+'suona una melodia triste' ,
+"suona un allarme" ,
+'mostra un numero' ,
+'smette di mostrare testi o numeri' ,
+'mostra del testo' ,
+'mostra un\'icona felice',
+'smette di mostrare un\'icona',
+'mostra un\'icona triste', 
+		    )
+
+microbitv2= ()
+
+
+# outputs_exBosonKit= ('spegne un ventilatore (BosonKit)' ,
+# 'accende un ventilatore (BosonKit)' ,
+# 'spegne una luce (BosonKit)',
+# 'accende una luce (BosonKit)',
+# 'fa ruotare il motore (BosonKit)' ,
+# 'smette di ruotare il motore (BosonKit)' ,
+# 'spegne un\'animazione luminosa (BosonKit)',
+# 'attiva un\'animazione luminosa (BosonKit)' ,
+# 'spegne un\'animazione luminosa verde (BosonKit)',
+# 'spegne un\'animazione luminosa rossa (BosonKit)',
+# 		     )
+
+exBosonKit= ('spegne un ventilatore (BosonKit)' ,
+'accende un ventilatore (BosonKit)' ,
+'spegne un\'animazione luminosa (BosonKit)',
+'attiva un\'animazione luminosa (BosonKit)' ,
+)
+
+exEnviroBit=()
+#"accende i LED bianchi (Envirobit)"
+#"spegne i LED bianchi (Envirobit)"
+
+exCloudBitPi= ()
+
+exOthers= ()
+
+p2p= ('invio dati',)	
