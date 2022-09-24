21fb7edb5d3f78f8e01adbdcd5b35d5f68c2a559
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-24 23:09:06 +0200

    seperated genURL

--- webapp_v3/main.py
+++ webapp_v3/main.py
@@ -11,7 +11,7 @@ from imageURL import imageURL
 from code_components import input_code, output_else_code, output_code 
 from code_components import package_suffix, input_sensorValue
 from code_components import on_end, on_start  
-
+from genURL import genURL
 
 
 import mods #for custom modifications to streamlit 
@@ -104,122 +104,6 @@ input1path=  baseURL+langPrefix[lang]+imageURL[ input_name[1]]
 output1path= baseURL+langPrefix[lang]+imageURL[output_name[1]]
     
  
-
-
-
-freeplaymode=True #legacy
-alwaysfreeplaymode=True #legacy
-
-
-
-def genURL (*args):#input_name, output_name):#here i am collecting chunks of code, encoding them, and concatenating them into a URL:
-     #----------on-start-code---------
-    on_start_code=[]
-    on_end_code=[]
-    jscode=""
-    for eachIOpair in args:
-        #print(eachIOpair)
-        for eachItem in eachIOpair:
-            #print(eachItem)
-            if True:#eachItem != "noInput" and eachItem != "noOutput": 
-                if eachItem in on_start:
-                    if eachItem=="sendData":
-                        on_start_code.append(on_start[eachItem].replace("1",groupnum)+ '\n')
-                    else:
-                        on_start_code.append(on_start[eachItem]+ '\n')
-                if eachItem in on_end:
-                    on_end_code.append(on_end[eachItem]+ '\n')
-    #print("onstart:",on_start_code)
-    on_start_code_noDup= list( dict.fromkeys(on_start_code) )
-    on_end_code_noDup= list( dict.fromkeys(on_end_code) )
-    #print("onstart_noDup:",on_start_code_noDup)    
-    for eachline in on_start_code_noDup:
-        jscode= jscode + eachline
-    jscode= jscode + 'basic.forever(function () {' + '\n'
-    #-----------if-else-code---------
-    if_body_code=""
-    if freeplaymode==True or alwaysfreeplaymode==True:
-        for eachIOpair in args: #in,out
-            if True:#eachIOpair[0] != "noInput" and eachIOpair[1] != "noOuput":
-                if eachIOpair[1] in output_code:
-                    if eachIOpair[1]=="sendData":
-                        if p2ptype=='invio dati': #gamelevel==0:
-                            if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[0][0:8])#.replace("inputValue","1")
-                        elif p2ptype=='ricevo dati': #gamelevel==1:
-                            if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[1][0:8])#.replace("inputValue","1")
-                        #if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[gamelevel]).replace("inputValue","1")
-                       ##if_body_code=output_code[eachIOpair[1]].replace("inputName",input_name[gamelevel]).replace("inputValue",input_sensorValue[input_name[gamelevel]])
-                    else:
-                        if_body_code=output_code[eachIOpair[1]]
-                if eachIOpair[1] in output_else_code:
-                    if eachIOpair[1]=="sendData":
-                        if p2ptype=='invio dati': #gamelevel==0:
-                            else_code=output_else_code[eachIOpair[1]].replace("inputName",input_name[0][0:8])   
-                        elif p2ptype=='ricevo dati': #gamelevel==1:
-                            else_code=output_else_code[eachIOpair[1]].replace("inputName",input_name[1][0:8])#.replace("inputValue","1")
-                    else:
-                        else_code = output_else_code[eachIOpair[1]]+ '\n'
-                else:
-                    else_code="basic.pause(100)"
-                if eachIOpair[0] in output_else_code:#special cases for forecast: get_temp
-                        else_code = output_else_code[eachIOpair[0]]+ '\n'
-
-
-                jscode= jscode  \
-                    + '    ' + 'if (' + input_code[eachIOpair[0]]+'){\n'  \
-                    + '    ' + '    ' + if_body_code +'\n'  \
-                    + '    ' + '} else {\n' \
-                    + '    ' + '    ' + else_code \
-                    + '    ' + '}\n'
-
-    else:
-
-        for eachIOpair in args: #in,out
-            if eachIOpair[0] != "noInput" and eachIOpair[1] != "noOutput":
-                if eachIOpair[1] in output_else_code:
-                    else_code = output_else_code[eachIOpair[1]]+ '\n'
-                else:
-                    else_code="basic.pause(1000)"
-                if eachIOpair[0] in output_else_code:#special cases for forecast: get_temp
-                        else_code = output_else_code[eachIOpair[0]]+ '\n'
-
-                jscode= jscode  \
-                    + '    ' + 'if (' + input_code[eachIOpair[0]]+'){\n'  \
-                    + '    ' + '    ' + output_code[eachIOpair[1]]+'\n'  \
-                    + '    ' + ' } else {\n' \
-                    + '    ' + '    ' + else_code +'\n' \
-                    + '    ' + '}\n'
-##+ '    ' + '    ' + 'basic.pause(1000)' +'\n' \
-
-
-
-                
-    #-----------closing forever loop---------
-    jscode=jscode+'})'
-    #-----------on_end_code---------
-    for eachline in on_end_code_noDup:
-        jscode= jscode + eachline 
-    #print(jscode)
-    
-    #------enclose jscode in URL:---    
-    url='https://makecode.microbit.org/--docs?md='+codetitle+codesubtitle+'%0A%0A%60%60%60%20blocks%0A'
-    for eachline in jscode:
-        url=url+urllib.parse.quote(eachline) 
-    url=url+'%0A%60%60%60%0A%0A'
-    #-----------add-extensions-code---------
-    on_end_code=[]
-    for eachIOpair in args:
-        for eachItem in eachIOpair: 
-            if True:#eachItem != "noInput" and eachItem != "noOutput":
-                if eachItem in package_suffix:
-                    on_end_code.append(package_suffix[eachItem])
-                #url=url+package_suffix[eachItem]
-    #print(on_end_code)
-    on_end_code_noDup= list( dict.fromkeys(on_end_code) )
-    #print(on_end_code_noDup)
-    for eachline in on_end_code_noDup:
-        url=url+eachline
-    return url, jscode
 
 
 
