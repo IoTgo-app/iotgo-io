fcfd500b25c385da91da74e1243b545660a6cb1c
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-25 22:05:09 +0200

    fix label

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -123,7 +123,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.button('➡ Back', key='backbutton')
 with col2:
-    st.button('➡ Cambia input oppure output fix label', key='editbutton')
+    st.button('➡ Cambia input oppure output', key='editbutton')
 
 # st.image(baseURL_codeSkeletons+skeleton+'.png')
 
