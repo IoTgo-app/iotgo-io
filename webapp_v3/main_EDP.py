d47da22d3f77a20c51b475a6cc40b81a0c72dd24
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-25 23:43:26 +0200

    simplified sidebar

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -144,21 +144,25 @@ col1, col2 = st.columns(2)
 
 with col1:
     # backbtn_placeholder=st.empty()
-    if st.button('⬅ Cambia esempio', key='backbutton' ):
-        with sidebar_placeholder:
-            skeleton=st.sidebar.selectbox('Select an example',skeleton_list)
-        # image_placeholder.image(baseURL_codeSkeletons+skeleton+'.png')
-        # skeleton=sidebar_placeholder.selectbox('Select an example',skeleton_list)
-        # # with sidebar_placeholder.container():
-        # #     inn,outt = sidebar_editExample()
-    else:
+    if not st.button('⬅ Cambia esempio', key='backbutton' ):
         with sidebar_placeholder:
             input0is=st.sidebar.selectbox('Select an input',['x','y'])
-            output0is=st.sidebar.selectbox('Select an output',['a','b'])
-        # input0is=st.sidebar.selectbox('Select an input',['x','y'])
-        # output0is=st.sidebar.selectbox('Select an output',['a','b'])
-        # # with sidebar_placeholder.container():
-            # st.sidebar.write('dup')
+            output0is=st.sidebar.selectbox('Select an output',['a','b'])        
+    # if st.button('⬅ Cambia esempio', key='backbutton' ):
+    #     with sidebar_placeholder:
+    #         skeleton=st.sidebar.selectbox('Select an example',skeleton_list)
+    #     # image_placeholder.image(baseURL_codeSkeletons+skeleton+'.png')
+    #     # skeleton=sidebar_placeholder.selectbox('Select an example',skeleton_list)
+    #     # # with sidebar_placeholder.container():
+    #     # #     inn,outt = sidebar_editExample()
+    # else:
+    #     with sidebar_placeholder:
+    #         input0is=st.sidebar.selectbox('Select an input',['x','y'])
+    #         output0is=st.sidebar.selectbox('Select an output',['a','b'])
+    #     # input0is=st.sidebar.selectbox('Select an input',['x','y'])
+    #     # output0is=st.sidebar.selectbox('Select an output',['a','b'])
+    #     # # with sidebar_placeholder.container():
+    #         # st.sidebar.write('dup')
             
 with col2:
     #st.button('➡ Cambia input oppure output', key='editbutton',on_click=editCodeSkeleton())
