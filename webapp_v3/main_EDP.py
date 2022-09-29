20fd6cb204178e3c8ef8e4a1619c850ca55a46cc
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-09-30 00:26:07 +0200

    value check

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -184,7 +184,8 @@ if sidebar_mode=="example_selected":
         if prevInput != st.session_state['input0is'] or prevOutput != st.session_state['output0is']:
                 # io_changed=True
                 # updateCode()
-            st.write("code updated")
+            st.write("code updated with "+=st.session_state['input0is']+" and "+=st.session_state['output0is'])
+
             st.balloons()
                 # io_changed=False
             prevInput=st.session_state['input0is']
@@ -210,7 +211,7 @@ elif sidebar_mode=="editing_example":
     # st.write("now we select inputs and outputs.")
 
 st.sidebar.markdown("---")
-st.sidebar.write("Stats for mehdi: programState = "+sidebar_mode+" \n- version 9.3 ")
+st.sidebar.write("Stats for mehdi: programState = "+sidebar_mode+" \n- version 9.4 ")
 st.session_state
 # with st.form("my_form"):
 #    st.write("Inside the form")
