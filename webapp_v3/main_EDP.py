737a7254304c5c7788f8146580472a6b7845e33b
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-10-03 23:37:23 +0200

    add var num

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -237,7 +237,7 @@ with code_placeholder:
     
 
 st.sidebar.markdown("---")
-st.sidebar.write("version 7.1.2")
+st.sidebar.write("version 7.1.3")
 st.session_state['io_list']  
 
 ########################### app end ########################################à
