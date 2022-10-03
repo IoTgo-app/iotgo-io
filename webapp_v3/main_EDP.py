236f29ce9d079a7dc2f296bdc6b46a5a7735a841
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-10-03 18:40:55 +0200

    test image URL

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -198,12 +198,12 @@ with code_placeholder:
     if st.session_state['sidebar_mode']=="app_start":
         st.empty()
     elif st.session_state['sidebar_mode']=="example_selected":
-        st.image(baseURL_cards+st.session_state['skeleton'][3:6]+'.png')
+        st.write(baseURL_cards+st.session_state['skeleton'][3:6]+'.png')
         st.markdown('[' + textIT['downloadProgram'] + '](' +urlis +')' , unsafe_allow_html=True)
         st.code(addDefaultIO(st.session_state['skeleton']), language="javascript")
         # st.image(baseURL_codeSkeletons+str(st.session_state['skeleton'])+'.png')
     elif st.session_state['sidebar_mode']=="editing_example":
-        st.image(baseURL_cards+st.session_state['skeleton'][3:6]+'.png')
+        st.write(baseURL_cards+st.session_state['skeleton'][3:6]+'.png')
         # st.image(baseURL_codeSkeletons+str(st.session_state['skeleton'])+'.png')
         # st.write("editing this....")
         st.markdown('[' + textIT['downloadProgram'] + '](' +urlis +')' , unsafe_allow_html=True)
@@ -213,7 +213,7 @@ with code_placeholder:
 
 
 st.sidebar.markdown("---")
-st.sidebar.write("version 6.9.0")
+st.sidebar.write("version 6.9.1")
 
 # st.session_state 
 
