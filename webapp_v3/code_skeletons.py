14bb97c6e2dc7cd95d8bdfdabe19b30d9621ca26
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-10-05 16:48:22 +0200

    remove extra var

--- webapp_v3/code_skeletons.py
+++ webapp_v3/code_skeletons.py
@@ -128,7 +128,6 @@ code_skeletons = {
         * IL DADO MUSICALE
         */
         basic.clearScreen()
-        let conto = 0
         let valore_caratteristico = 0
         basic.forever(function () {
             basic.pause(2000)
@@ -256,7 +255,6 @@ code_skeletons = {
         * IL DADO MUSICALE, ALTRIMENTI SPENTO
         */
         basic.clearScreen()
-        let conto = 0
         let valore_caratteristico = 0
 
         basic.forever(function () {
@@ -272,18 +270,6 @@ code_skeletons = {
 }
 
 
-
-# def addDefaultIO(skeleton_name):
-#     currentSkeleton=code_skeletons[skeleton_name]
-#     IO2add=default_IO[skeleton_name]
-#     for eachKey, eachVal in IO2add.items():
-#         if eachKey=="in1" or eachKey=="in2":
-#             currentSkeleton=currentSkeleton.replace("_"+eachKey+"_", input_code[eachVal])
-#         if eachKey=="out1" or eachKey=="out2":
-#             currentSkeleton=currentSkeleton.replace("_"+eachKey+"_", output_code[eachVal])
-#         if eachKey=="out1else" or eachKey=="out2else":
-#             currentSkeleton=currentSkeleton.replace("_"+eachKey+"_", output_else_code[eachVal])
-#     return currentSkeleton
 
 
 def changeIO(skeleton_name,IO2change):
@@ -306,238 +292,238 @@ def changeIO(skeleton_name,IO2change):
 
 
 
-code_examples = {
-    "lvl1a1-1in_1out" : 
-        """
-        /**
-        * CESTINO SORRIDENTE
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            }
-        })
-        """, 
-    "lvl1b1-1in_2out" : 
-        """
-        /**
-        * ALBERO ANIMATO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Heart)
-                basic.showIcon(IconNames.SmallHeart)
-            }
-        })
-        """, 
-    "lvl1c1-2in_1out" : 
-        """
-        /**
-        * OROLOGIO SEGNAFREDDO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A) && input.temperature() < 10) {
-                basic.showIcon(IconNames.Sad)
-            }
-        })
-        """, 
-    "lvl2a1-1in_1out-1in_1out" : 
-        """
-                /**
-        * TAVOLO GIOCO X 2, FACCINA E MUSICA
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            }
-            if (input.buttonIsPressed(Button.B)) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            }
-        })
-        """, 
-    "lvl2b1-1in_1out_elif_1in_1out" : 
-        """
-                /**
-        * CAVALLO SFIDA X 2, O  FACCINA O MUSICA
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            } else if (input.buttonIsPressed(Button.B)) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            }
-        })
-        """, 
-    "lvl2c1-1in_1out-nest-1in_1out" : 
-        """
-        /**
-        * PANCHINA x DUE., CON FACCINA E POI MUSICALE
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-                if (input.buttonIsPressed(Button.B)) {
-                    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-                }
-            }
-        })
-        """, 
-    "lvl3a1-1in_1out_var" : 
-        """
-                /**
-        * IL CANESTRO MOSTRA-PUNTEGGIO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                conto += 1
-                basic.showString("" + (conto))
-            }
-        })
-        """, 
-    "lvl3b1-1var_1out" : 
-        """
-        /**
-        * IL DADO MUSICALE
-        */
-        basic.clearScreen()
-        let conto = 0
-        let valore_caratteristico = 0
-        basic.forever(function () {
-            basic.pause(2000)
-            valore_caratteristico += randint(1, 6)
-            basic.showNumber(valore_caratteristico)
-            if (valore_caratteristico % 2 == 0) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            }
-        })
-        """, 
-    "lvl1a2-1in_1out_else" : 
-        """
-        /**
-        * ALBERO ANIMATO, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Heart)
-                basic.showIcon(IconNames.SmallHeart)
-            } else {
-                basic.clearScreen()
-            }
-        })
-        """, 
-    "lvl1b1-1in_2out_else" : 
-        """
-        /**
-        * CESTINO SORRIDENTE, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            } else {
-                basic.clearScreen()
-            }
-        }) 
-        """, 
-    "lvl1c2-2in_1out_else" : 
-        """
-                /**
-        * OROLOGIO SEGNAFREDDO, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A) && input.temperature() < 10) {
-                basic.showIcon(IconNames.Sad)
-            } else {
-                basic.clearScreen()
-            }
-        })
-        """, 
-    "lvl2a2-1in_1out_else-1in_1out_else" : 
-        """
-        /**
-        * TAVOLO GIOCO X 2, FACCINA E MUSICA, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            } else {
-                basic.clearScreen()
-            }
-            if (input.buttonIsPressed(Button.B)) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            } else {
-                music.stopAllSounds()
-            }
-        })
-        """, 
-    "lvl2b2-1in_1out_elif_1in_1out_else" : 
-        """
-        /**
-        * CAVALLO SFIDA X 2, O  FACCINA O MUSICA, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-            } else if (input.buttonIsPressed(Button.B)) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            } else {
-                basic.clearScreen()
-                music.stopAllSounds()
-            }
-        })
-        """, 
-    "lvl2c2-1in_1out-nest-1in_1out_else_else" : 
-        """
-        /**
-        * PANCHINA x DUE., CON FACCINA E POI MUSICALE, ALTRIMENTI SPENTA
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                basic.showIcon(IconNames.Happy)
-                if (input.buttonIsPressed(Button.B)) {
-                    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-                } else {
-                    music.stopAllSounds()
-                }
-            } else {
-                basic.clearScreen()
-            }
-        })
-        """, 
-    "lvl3a2-1in_1out_var_else" : 
-        """
-        /**
-        * IL CANESTRO MOSTRA-PUNTEGGIO, ALTRIMENTI SPENTO
-        */
-        basic.forever(function () {
-            if (input.buttonIsPressed(Button.A)) {
-                conto += 1
-                basic.showString("" + (conto))
-            } else {
-                basic.clearScreen()
-            }
-        })
-        """, 
-    "lvl3b2-1var_1out_else" : 
-        """   
-        /**
-        * IL DADO MUSICALE, ALTRIMENTI SPENTO
-        */
-        basic.clearScreen()
-        let conto = 0
-        let valore_caratteristico = 0
+# code_examples = {
+#     "lvl1a1-1in_1out" : 
+#         """
+#         /**
+#         * CESTINO SORRIDENTE
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             }
+#         })
+#         """, 
+#     "lvl1b1-1in_2out" : 
+#         """
+#         /**
+#         * ALBERO ANIMATO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Heart)
+#                 basic.showIcon(IconNames.SmallHeart)
+#             }
+#         })
+#         """, 
+#     "lvl1c1-2in_1out" : 
+#         """
+#         /**
+#         * OROLOGIO SEGNAFREDDO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A) && input.temperature() < 10) {
+#                 basic.showIcon(IconNames.Sad)
+#             }
+#         })
+#         """, 
+#     "lvl2a1-1in_1out-1in_1out" : 
+#         """
+#                 /**
+#         * TAVOLO GIOCO X 2, FACCINA E MUSICA
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             }
+#             if (input.buttonIsPressed(Button.B)) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             }
+#         })
+#         """, 
+#     "lvl2b1-1in_1out_elif_1in_1out" : 
+#         """
+#                 /**
+#         * CAVALLO SFIDA X 2, O  FACCINA O MUSICA
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             } else if (input.buttonIsPressed(Button.B)) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             }
+#         })
+#         """, 
+#     "lvl2c1-1in_1out-nest-1in_1out" : 
+#         """
+#         /**
+#         * PANCHINA x DUE., CON FACCINA E POI MUSICALE
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#                 if (input.buttonIsPressed(Button.B)) {
+#                     music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#                 }
+#             }
+#         })
+#         """, 
+#     "lvl3a1-1in_1out_var" : 
+#         """
+#                 /**
+#         * IL CANESTRO MOSTRA-PUNTEGGIO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 conto += 1
+#                 basic.showString("" + (conto))
+#             }
+#         })
+#         """, 
+#     "lvl3b1-1var_1out" : 
+#         """
+#         /**
+#         * IL DADO MUSICALE
+#         */
+#         basic.clearScreen()
+#         let conto = 0
+#         let valore_caratteristico = 0
+#         basic.forever(function () {
+#             basic.pause(2000)
+#             valore_caratteristico += randint(1, 6)
+#             basic.showNumber(valore_caratteristico)
+#             if (valore_caratteristico % 2 == 0) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             }
+#         })
+#         """, 
+#     "lvl1a2-1in_1out_else" : 
+#         """
+#         /**
+#         * ALBERO ANIMATO, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Heart)
+#                 basic.showIcon(IconNames.SmallHeart)
+#             } else {
+#                 basic.clearScreen()
+#             }
+#         })
+#         """, 
+#     "lvl1b1-1in_2out_else" : 
+#         """
+#         /**
+#         * CESTINO SORRIDENTE, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             } else {
+#                 basic.clearScreen()
+#             }
+#         }) 
+#         """, 
+#     "lvl1c2-2in_1out_else" : 
+#         """
+#                 /**
+#         * OROLOGIO SEGNAFREDDO, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A) && input.temperature() < 10) {
+#                 basic.showIcon(IconNames.Sad)
+#             } else {
+#                 basic.clearScreen()
+#             }
+#         })
+#         """, 
+#     "lvl2a2-1in_1out_else-1in_1out_else" : 
+#         """
+#         /**
+#         * TAVOLO GIOCO X 2, FACCINA E MUSICA, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             } else {
+#                 basic.clearScreen()
+#             }
+#             if (input.buttonIsPressed(Button.B)) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             } else {
+#                 music.stopAllSounds()
+#             }
+#         })
+#         """, 
+#     "lvl2b2-1in_1out_elif_1in_1out_else" : 
+#         """
+#         /**
+#         * CAVALLO SFIDA X 2, O  FACCINA O MUSICA, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#             } else if (input.buttonIsPressed(Button.B)) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             } else {
+#                 basic.clearScreen()
+#                 music.stopAllSounds()
+#             }
+#         })
+#         """, 
+#     "lvl2c2-1in_1out-nest-1in_1out_else_else" : 
+#         """
+#         /**
+#         * PANCHINA x DUE., CON FACCINA E POI MUSICALE, ALTRIMENTI SPENTA
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 basic.showIcon(IconNames.Happy)
+#                 if (input.buttonIsPressed(Button.B)) {
+#                     music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#                 } else {
+#                     music.stopAllSounds()
+#                 }
+#             } else {
+#                 basic.clearScreen()
+#             }
+#         })
+#         """, 
+#     "lvl3a2-1in_1out_var_else" : 
+#         """
+#         /**
+#         * IL CANESTRO MOSTRA-PUNTEGGIO, ALTRIMENTI SPENTO
+#         */
+#         basic.forever(function () {
+#             if (input.buttonIsPressed(Button.A)) {
+#                 conto += 1
+#                 basic.showString("" + (conto))
+#             } else {
+#                 basic.clearScreen()
+#             }
+#         })
+#         """, 
+#     "lvl3b2-1var_1out_else" : 
+#         """   
+#         /**
+#         * IL DADO MUSICALE, ALTRIMENTI SPENTO
+#         */
+#         basic.clearScreen()
+#         let conto = 0
+#         let valore_caratteristico = 0
 
-        basic.forever(function () {
-            basic.pause(2000)
-            valore_caratteristico += randint(1, 6)
-            basic.showNumber(valore_caratteristico)
-            if (valore_caratteristico % 2 == 0) {
-                music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-            } else {
-                music.stopAllSounds()
-            }
-        })
-        """
-}
+#         basic.forever(function () {
+#             basic.pause(2000)
+#             valore_caratteristico += randint(1, 6)
+#             basic.showNumber(valore_caratteristico)
+#             if (valore_caratteristico % 2 == 0) {
+#                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+#             } else {
+#                 music.stopAllSounds()
+#             }
+#         })
+#         """
+# }
 
 
 
