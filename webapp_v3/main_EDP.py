2f9b3f042e2480e8ce26c59fb6e6b5cfbb60af1d
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-10-12 15:27:58 +0200

    add all card images

--- webapp_v3/main_EDP.py
+++ webapp_v3/main_EDP.py
@@ -161,14 +161,13 @@ with cardDeck_placeholder:
             st.session_state['input1index'] = input_options.index(en2it_inout[st.session_state['io_list']['in1']])
             # st.session_state['input1index'] = input_options.index(en2it_inout[st.session_state['io_list']['in1']])
             st.session_state['input0is'] =st.selectbox( textIT['selectInput1'],input_options,index= int(st.session_state['input1index']))#,index=2) #,key='selInput')
+            st.image(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['input0is']]], width=cardWidth) 
             if not st.session_state['prevInput']==st.session_state['input0is']:  
                 st.session_state['prevInput']=st.session_state['input0is']
                 temp=default_IO[st.session_state['skeleton']]
                 temp["in1"]=it2en_inout[st.session_state['input0is']]
                 st.session_state['io_list']  = temp
-                changeIO(st.session_state['skeleton'],st.session_state['io_list'])
-                st.image(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['input0is']]], width=cardWidth) 
-                st.write(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['input0is']]], width=cardWidth)             
+                changeIO(st.session_state['skeleton'],st.session_state['io_list'])            
                 # st.session_state['changingCardsNow'] = "in1"
                 st.experimental_rerun()
         elif st.session_state['nav_list'][st.session_state['io_index']+1] == "in2":
@@ -176,6 +175,7 @@ with cardDeck_placeholder:
             # if "in2" in st.session_state['io_list'].keys():
             st.session_state['input2index'] = input_options.index(en2it_inout[st.session_state['io_list']['in2']])
             st.session_state['input2is'] =st.selectbox( textIT['selectInput2'],input_options,index= int(st.session_state['input2index']))#,index=2) #,key='selInput')
+            st.image(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['input2is']]], width=cardWidth) 
             if not st.session_state['prevInput2']==st.session_state['input2is']:  
                 st.session_state['prevInput2']=st.session_state['input2is']
                 temp=default_IO[st.session_state['skeleton']]
@@ -187,6 +187,7 @@ with cardDeck_placeholder:
             # if "out1" in st.session_state['io_list'].keys():
             st.session_state['output1index'] = output_options.index(en2it_inout[st.session_state['io_list']['out1']])
             st.session_state['output0is'] =st.selectbox( textIT['selectOutput1'],output_options,index= int(st.session_state['output1index']))#,key='selInput')
+            st.image(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['output0is']]], width=cardWidth) 
             if not st.session_state['prevOutput']==st.session_state['output0is']:
                 st.session_state['prevOutput']=st.session_state['output0is']
                 temp=default_IO[st.session_state['skeleton']]
@@ -198,6 +199,7 @@ with cardDeck_placeholder:
             # if "out2" in st.session_state['io_list'].keys():
             st.session_state['output2index'] = output_options.index(en2it_inout[st.session_state['io_list']['out2']])
             st.session_state['output2is'] =st.selectbox( textIT['selectOutput2'],output_options,index= int(st.session_state['output2index']))#,key='selInput')
+            st.image(baseURL+langPrefix[lang]+imageURL[it2en_inout[st.session_state['output0is']]], width=cardWidth) 
             if not st.session_state['prevOutput2']==st.session_state['output2is']:
                 st.session_state['prevOutput2']=st.session_state['output2is']
                 temp=default_IO[st.session_state['skeleton']]
@@ -329,7 +331,7 @@ with download_placeholder:
 
 ########################### app end ########################################à
 st.sidebar.markdown("---")
-st.sidebar.write("version 7.7.4 testing new nav")
+st.sidebar.write("version 7.7.5 testing new nav")
 st.session_state['io_index']
 st.session_state['sidebar_mode']
 
