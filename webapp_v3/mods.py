99c7f06156cd3d3da7a3575927dcf07f432d5ac0
IoTgo-app <88254723+IoTgo-app@users.noreply.github.com>
2022-10-14 23:53:35 +0100

    reize sidebar 350px

--- webapp_v3/mods.py
+++ webapp_v3/mods.py
@@ -16,8 +16,8 @@ button[title="View fullscreen"]{
 
 fix_sidebar= """
 <style>
-[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 320px;}
-[data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 320px;margin-left: -320px;}
+[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 350px;}
+[data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 350px;margin-left: -350px;}
 </style>
 """
 
